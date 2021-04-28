54c1f08e3366bea5ab12e22c30b2372b4e23d6c7
Paccy10 <pacifiqueclement@gmail.com>
2021-04-28 18:32:09 +0200

    Added working list tags endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'core',
-    'users'
+    'users',
+    'recipes'
 ]
 
 MIDDLEWARE = [
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,17 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@recipeapp.com', password='testpass'):
+    """ Create a sample user """
+
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
+    """ Models tests class """
 
     def test_create_user_with_email_successful(self):
         """ Test creating a new user with an email is successfull """
@@ -35,3 +44,10 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """ Test the tag string representation """
+
+        tag = models.Tag.objects.create(user=sample_user(), name='Vegan')
+
+        self.assertEqual(str(tag), tag.name)
